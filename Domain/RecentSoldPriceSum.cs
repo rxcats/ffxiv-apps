0b11787813a45eac95b56b95b1a60c88624d101c
rxcats <windpyj80@gmail.com>
2025-07-09 05:53:04 +0900

    고잉마샤호 최근 총수입 확인 기능 추가

---
+++ Domain/RecentSoldPriceSum.cs
@@ -0,0 +1,9 @@
+﻿namespace ffxiv_apps.Domain;
+
+public class RecentSoldPriceSum
+{
+    public string YearMonth { get; init; } = null!;
+    public int PriceSum { get; init; }
+
+    public string YearMonthFormat => YearMonth.Replace("-", "/");
+}
